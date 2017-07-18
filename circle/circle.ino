b6438be25495aa5f50ae965f72a3bc85d32e28e2
chand1012 <chandlerl14@outlook.com>
2017-07-18 00:43:05 -0400

    Arduino moved things

---
+++ circle/circle.ino
@@ -0,0 +1,19 @@
+//this is the code for the car going round in circles
+#include <AFMotor.h>
+AF_DCMotor drive(1);
+AF_DCMotor steer(3);
+void setup() {
+  Serial.begin(9600);
+  drive.setSpeed(255);
+  steer.setSpeed(255);
+}
+
+void loop() {
+  drive.run(FORWARD);
+  steer.run(FORWARD);
+  delay(30000);
+  steer.run(RELEASE);
+  drive.run(RELEASE);
+  delay(5000);
+
+}
